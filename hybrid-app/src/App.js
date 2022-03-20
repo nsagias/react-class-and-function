e04ca5c5e61b9ab378196fdfe4c6467ae3e6eb13
Nick <nsagias@gmail.com>
2022-03-19 22:31:20 -0400

    convert to basic hello world

--- hybrid-app/src/App.js
+++ hybrid-app/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
