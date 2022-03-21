2855ca3add84b16f48a0accee1e9c86314ed8306
Nick <nsagias@gmail.com>
2022-03-21 02:22:25 -0400

    add onclick handler

--- hybrid-app/src/components/DebounceButtons.jsx
+++ hybrid-app/src/components/DebounceButtons.jsx
@@ -1,6 +1,12 @@
 import React, {useEffect, useState} from "react";
 
 export default function DebounceButton () {
+
+  const onClickleDebounceButton = (event) => {
+    event.preventDefault();
+    console.log("DB buttton");
+  }
+
   return (
     <>
       <button>D-Button</button>
